c2cafc64674d57476a5e136ed077058edcb12258
Diksha220 <60913047+Diksha220@users.noreply.github.com>
2021-01-13 12:28:05 +0530

    sidebar complete

---
+++ src/component/sidebar/sidebar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <>
+            <div className='sidebar'>
+            <div className='iconsA'>
+              <img src="./images/Address-book-icon.svg" alt="ad-book" />
+              <img src="./images/paper-plane.svg" alt="plane" />
+              <img src="./images/inbox.svg" alt="inbox" />
+              <img src="./images/prospects.svg" alt="prospects" />
+              <img src="./images/automation.svg" alt="automation" />
+              <img src="./images/report.svg" alt="report" />
+          </div>
+            <div className='iconsB'>
+                <img src="./images/billing.svg" alt="billing" />
+                <img src="./images/integration.svg" alt="integration" />
+                <img src="./images/noti.svg" alt="noti" />
+                <img src="./images/profile.svg" alt="profile" />
+            </div>
+            </div>
+        </>
+    );
+  }
+  export default Sidebar;
